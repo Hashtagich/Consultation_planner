f25fb71d38402c1aba5a056280b19c222520df5a
Hashtagich <antonchikz@mail.ru>
2024-10-01 20:05:18 +0300

    Docker. Точку отчёта, работа с нджинкс.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -17,12 +17,12 @@ python3 manage.py collectstatic --no-input --clear
 
 # Проверка существования суперпользователя
 # Проверка существования суперпользователя
-if python3 manage.py shell -c "from users.models import CustomUser; print(CustomUser.objects.filter(is_superuser=True, email='$SUPERUSER_EMAIL').exists())" | grep -q True; then
-    echo "Superuser already exists"
-else
-    # Создание суперпользователя
-    echo "Creating superuser..."
-    python3 manage.py createsuperuser --noinput --email "$SUPERUSER_EMAIL" --middle_name "$SUPERUSER_MIDDLE_NAME" --first_name "$SUPERUSER_FIRST_NAME" --password "$SUPERUSER_PASSWORD"
-fi
+#if python3 manage.py shell -c "from users.models import CustomUser; print(CustomUser.objects.filter(is_superuser=True, email='$SUPERUSER_EMAIL').exists())" | grep -q True; then
+#    echo "Superuser already exists"
+#else
+#    # Создание суперпользователя
+#    echo "Creating superuser..."
+#    python3 manage.py createsuperuser --noinput --email "$SUPERUSER_EMAIL" --middle_name "$SUPERUSER_MIDDLE_NAME" --first_name "$SUPERUSER_FIRST_NAME" --password "$SUPERUSER_PASSWORD"
+#fi
 
 exec "$@"
